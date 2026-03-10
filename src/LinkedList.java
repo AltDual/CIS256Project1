12fbadf08c963c377672d3d122eb274b3449d837
Taiki Shinokawa <ts3078541@gmail.com>
2026-03-10 11:56:03 -0700

    added all insertation search some removal

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -12,6 +12,18 @@ public class LinkedList<T> {
     this.tail = node;
   }
 
+  public LinkedListNode<T> search(T data) {
+    LinkedListNode<T> currentNode = this.head;
+    while (currentNode != null) {
+      if (currentNode.data.equals(data)) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  // All insertation operations
   public void append(T data) {
     LinkedListNode<T> newNode = new LinkedListNode<T>(data);
     if (tail == null) {
@@ -33,5 +45,47 @@ public class LinkedList<T> {
       this.head = newNode;
     }
   }
-  // TODO: Add insertNodeAfter, removeNode, removeNodeAfter
+
+  public boolean insertAfter(T nodeData, T dataToInsert) {
+    LinkedListNode<T> node = search(nodeData);
+    if (node != null) {
+      LinkedListNode<T> newNode = new LinkedListNode<T>(dataToInsert);
+      insertNodeAfter(node, newNode);
+      return true;
+    }
+    return false;
+  }
+
+  private void insertNodeAfter(LinkedListNode<T> currentNode, LinkedListNode<T> newNode) {
+    if (head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else if (currentNode == tail) {
+      currentNode.next = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = currentNode.next;
+      currentNode.next = newNode;
+    }
+  }
+
+  // All removal opertaions
+  public boolean removeNode(T data) {
+    LinkedListNode<T> previous = null;
+    LinkedListNode<T> current = this.head;
+
+    while (current != null) {
+      if (current.data.equals(data)) {
+        removeNodeAfter(previous);
+      }
+      previous = current;
+      current = current.next;
+    }
+    return false;
+  }
+
+  // TODO: Finish implementing method below
+  public void removeNodeAfter(LinkedListNode<T> node) {
+
+  }
 }
