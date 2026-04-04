4461926c4bb97d96397e90f92ddfbe59b0282a76
Taiki Shinokawa <ts3078541@gmail.com>
2026-04-03 20:56:05 -0700

    need to fix the App line 19 with the scanner

--- src/App.java
+++ src/App.java
@@ -1,22 +1,48 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.HashSet;
 
 public class App {
   public static void main(String[] args) {
-    HashMap<String, ArrayList<String>> documents = new HashMap<String, ArrayList<String>>();
+    System.out.println("Program Initialized");
+    HashMap<String, HashSet<String>> documents = new HashMap<String, HashSet<String>>();
     processDocuments(documents);
+    while (true) {
+      System.out.println("What word do you want to search for: ");
+      queryDocuments(documents);
+    }
+  }
+
+  public static void queryDocuments(HashMap<String, HashSet<String>> documents) {
+    Scanner scnr = new Scanner(System.in);
+    ArrayList<String> strings = new ArrayList<String>();
+    String s = scnr.nextLine();
+    Scanner stringScnr = new Scanner(s);
+    while (stringScnr.hasNext()) {
+      strings.add(stringScnr.next());
+    }
+    HashSet<String> docsToReturn = new HashSet<String>();
+    for (int i = 0; i < strings.size(); i++) {
+      HashSet<String> set = new HashSet<String>(documents.search(strings.get(i)));
+      docsToReturn.retainAll(set);
+    }
+    System.out.println(docsToReturn);
+    scnr.close();
+    stringScnr.close();
   }
 
   // Change filepath accordingly for your setup
-  public static void processDocuments(HashMap<String, ArrayList<String>> map) {
+  public static void processDocuments(HashMap<String, HashSet<String>> map) {
     Tokenizer t = new Tokenizer("documents/doc1.txt");
     LinkedList<String> l = t.tokenize();
     LinkedListNode<String> current = l.head;
     while (current != null) {
-      ArrayList<String> s = new ArrayList<String>();
+      HashSet<String> s = new HashSet<String>();
       s.add("doc1.txt");
       map.insert(current.data, s);
       current = current.next;
     }
+    System.out.println("Finished doc1");
     t = new Tokenizer("documents/doc2.txt");
     l = t.tokenize();
     current = l.head;
@@ -24,11 +50,13 @@ public class App {
       if (map.search(current.data) != null) {
         map.search(current.data).add("doc2.txt");
       } else {
-        ArrayList<String> s = new ArrayList<String>();
+        HashSet<String> s = new HashSet<String>();
         s.add("doc2.txt");
         map.insert(current.data, s);
       }
+      current = current.next;
     }
+    System.out.println("Finished doc2");
     t = new Tokenizer("documents/doc3.txt");
     l = t.tokenize();
     current = l.head;
@@ -36,11 +64,13 @@ public class App {
       if (map.search(current.data) != null) {
         map.search(current.data).add("doc3.txt");
       } else {
-        ArrayList<String> s = new ArrayList<String>();
+        HashSet<String> s = new HashSet<String>();
         s.add("doc3.txt");
         map.insert(current.data, s);
       }
+      current = current.next;
     }
+    System.out.println("Finished doc3");
     t = new Tokenizer("documents/doc4.txt");
     l = t.tokenize();
     current = l.head;
@@ -48,10 +78,12 @@ public class App {
       if (map.search(current.data) != null) {
         map.search(current.data).add("doc4.txt");
       } else {
-        ArrayList<String> s = new ArrayList<String>();
+        HashSet<String> s = new HashSet<String>();
         s.add("doc4.txt");
         map.insert(current.data, s);
       }
+      current = current.next;
     }
+    System.out.println("Finished doc4");
   }
 }
